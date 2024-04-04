bbf09bc6c641aa22cd2b0659979419c8a639d517
Sai Praneeth Mamillapalli <saiwork@Sais-MacBook-Pro.local>
2024-04-03 21:59:03 -0500

    fizzbuzz powershell

---
+++ katafizz.ps1
@@ -0,0 +1,19 @@
+for ($i = 1; $i -lt 100; $i++) {
+
+    if($i % 3 -eq 0 -and $i % 5 -eq 0){
+        echo "fizzbuzz"
+    }
+    elseif ($i % 3 -eq 0 ) {
+        echo "fizz"
+    }
+    elseif($i % 5 -eq 0 ){
+        echo "buzz"
+    }
+    else{
+        echo $i
+    }
+    
+
+
+    
+}
